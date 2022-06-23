13842f6b170a69d969085e6e3bf180e8156f903b
Cody Pronnier <codypronnier@gmail.com>
2022-06-23 22:42:47 +0200

    test async 5

--- utilisateur.test.js
+++ utilisateur.test.js
@@ -15,35 +15,35 @@ describe('creation utilisateur', () => {
         });
     });
   
-    // it('creation et sauvegarde dun utilisateur', async () => {
-    //     const validUser = new UtilisateurModel(userData);
-    //     const savedUser = await validUser.save();
-    //     // Object Id should be defined when successfully saved to MongoDB.
-    //     expect(savedUser._id).toBeDefined();
-    //     expect(savedUser.nom).toBe(userData.nom);
-    //     expect(savedUser.prenom).toBe(userData.prenom);
-    //     expect(savedUser.pseudo).toBe(userData.pseudo);
-    //     expect(savedUser.mail).toBe(userData.mail);
-    //     expect(savedUser.description).toBe(userData.description);
-    // });
+    it('creation et sauvegarde dun utilisateur', async () => {
+        const validUser = new UtilisateurModel(userData);
+        const savedUser = await validUser.save();
+        // Object Id should be defined when successfully saved to MongoDB.
+        expect(savedUser._id).toBeDefined();
+        expect(savedUser.nom).toBe(userData.nom);
+        expect(savedUser.prenom).toBe(userData.prenom);
+        expect(savedUser.pseudo).toBe(userData.pseudo);
+        expect(savedUser.mail).toBe(userData.mail);
+        expect(savedUser.description).toBe(userData.description);
+    });
 
-    // it('insertion dun utilisateur réussie mais un champ nest pas défini', async () => {
-    //     const utilisateuravecchampinvalide = new UtilisateurModel({nom: 'Dylan', prenom: 'Bob', mail: 'test@demo.fr', mot_de_passe: 'testtest',  pseudo: "bobo",  });
-    //     const sauvegardeutilisateuravecchampinvalide = await utilisateuravecchampinvalide.save();
-    //     expect(sauvegardeutilisateuravecchampinvalide._id).toBeDefined();
-    //     expect(sauvegardeutilisateuravecchampinvalide.description).toBeUndefined();
-    // });
+    it('insertion dun utilisateur réussie mais un champ nest pas défini', async () => {
+        const utilisateuravecchampinvalide = new UtilisateurModel({nom: 'Dylan', prenom: 'Bob', mail: 'test@demo.fr', mot_de_passe: 'testtest',  pseudo: "bobo",  });
+        const sauvegardeutilisateuravecchampinvalide = await utilisateuravecchampinvalide.save();
+        expect(sauvegardeutilisateuravecchampinvalide._id).toBeDefined();
+        expect(sauvegardeutilisateuravecchampinvalide.description).toBeUndefined();
+    });
 
-    // it('création dun utilisateur avec un champ non défini mais obligatoire', async () => {
-    //     const utilisateuravecchampnondefinimaisobligatoire = new UtilisateurModel({nom: 'Dylan', prenom: 'Bob', mot_de_passe: 'testtest',  pseudo: "bobo"});
-    //     let err;
-    //     try {
-    //         const sauvegardeutilisateuravecchampnondefinimaisobligatoire = await utilisateuravecchampnondefinimaisobligatoire.save();
-    //         error = sauvegardeutilisateuravecchampnondefinimaisobligatoire;
-    //     } catch (error) {
-    //         err = error
-    //     }
-    //     expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
-    //     expect(err.errors.mail).toBeDefined();
-    // });  
+    it('création dun utilisateur avec un champ non défini mais obligatoire', async () => {
+        const utilisateuravecchampnondefinimaisobligatoire = new UtilisateurModel({nom: 'Dylan', prenom: 'Bob', mot_de_passe: 'testtest',  pseudo: "bobo"});
+        let err;
+        try {
+            const sauvegardeutilisateuravecchampnondefinimaisobligatoire = await utilisateuravecchampnondefinimaisobligatoire.save();
+            error = sauvegardeutilisateuravecchampnondefinimaisobligatoire;
+        } catch (error) {
+            err = error
+        }
+        expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
+        expect(err.errors.mail).toBeDefined();
+    });  
   });
