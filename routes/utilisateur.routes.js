ee3d10efa887ebacd8acd3f8dc1a050d1b520544
Cody Pronnier <codypronnier@gmail.com>
2022-07-02 17:01:45 +0200

    auth ressource

--- routes/utilisateur.routes.js
+++ routes/utilisateur.routes.js
@@ -7,14 +7,14 @@ const multer = require('multer')
 const sharp = require('sharp')
 
 router.get("/utilisateur",auth , utilisateurController.afficherUtilisateurs);
-router.post("/utilisateur", utilisateurController.ajoutUtilisateur);
-
+router.post("/utilisateur",  utilisateurController.ajoutUtilisateur);
 router.post("/connexion", utilisateurController.Login);
 router.post("/deconnexion", auth, utilisateurController.Logout);
 router.get("/utilisateur/profil", auth, utilisateurController.profil);
 router.get("/utilisateur/:id/ressources",auth, utilisateurController.toutesRessourcesDeUtilisateur);
 router.patch("utilisateur/:id/switch", auth, utilisateurController.switchCompteUtilisateur);
 router.patch("utilisateur/:id/follow", auth, utilisateurController.follow);
+router.patch("utilisateur/:id", auth, utilisateurController.modifUtilisateur);
 
 const upload = multer({
     limits: {
