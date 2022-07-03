c4601b5911c1aaf537105758711c7736ac1ba9c6
Cody Pronnier <codypronnier@gmail.com>
2022-07-03 13:39:11 +0200

    test

--- controllers/utilisateur.controller.js
+++ controllers/utilisateur.controller.js
@@ -1,59 +1,52 @@
-const UtilisateurModel = require("../models/Utilisateur");
-const sharp = require("sharp");
+const UtilisateurModel = require('../models/Utilisateur')
+const sharp = require('sharp')
+
 
 // Affiche tous les utilisateurs
 const afficherUtilisateurs = async (req, res) => {
-  const users = await UtilisateurModel.find({});
-  res.send(users);
-};
+    const users = await UtilisateurModel.find({});
+    res.send(users);
+  };
 
-const ajoutUtilisateur = async (req, res) => {
-  const utilisateur = new UtilisateurModel(req.body);
-  await utilisateur.save();
-  res.send(utilisateur);
-};
-
-const ajoutUtilisateurInscription = async (req, res) => {
-  const utilisateur = new UtilisateurModel({
-    nom: req.body.nom,
-    prenom: req.body.prenom,
-    mot_de_passe: req.body.mot_de_passe,
-    pseudo: req.body.pseudo,
-    mail: req.body.mail,
-    image: req.file.path.substring(14)
-  });
-  try {
+  const ajoutUtilisateur = async (req, res) => {
+    const utilisateur = new UtilisateurModel(req.body);
     await utilisateur.save();
-    const token = await utilisateur.generateAuthToken();
-    res.status(201).send({ utilisateur, token });
-  } catch (e) {
-    res.status(400).send(e);
-  }
+    res.send(utilisateur);
+  };
+
+
+  const ajoutUtilisateurInscription = async (req, res) => {
+    const utilisateur = new UtilisateurModel(req.body);
+    try {
+        await utilisateur.save();
+        const token = await utilisateur.generateAuthToken();
+        res.status(201).send({ utilisateur, token });
+    } catch (e) {
+        res.status(400).send(e);
+    }
 };
+
 
 const Login = async (req, res) => {
   try {
-    const utilisateur = await UtilisateurModel.findByCredentials(
-      req.body.mail,
-      req.body.mot_de_passe
-    );
-    const token = await utilisateur.generateAuthToken();
-    res.send({ utilisateur, token });
+      const utilisateur = await UtilisateurModel.findByCredentials(req.body.mail, req.body.mot_de_passe);
+      const token = await utilisateur.generateAuthToken();
+      res.send({ utilisateur, token })
   } catch (e) {
-    res.status(400).send();
+      res.status(400).send()
   }
 };
 
 const Logout = async (req, res) => {
   try {
-    req.utilisateur.tokens = req.utilisateur.tokens.filter((token) => {
-      return token.token !== req.token;
-    });
-    await req.utilisateur.save();
+      req.utilisateur.tokens = req.utilisateur.tokens.filter((token) => {
+          return token.token !== req.token;
+      })
+      await req.utilisateur.save();
 
-    res.send();
+      res.send();
   } catch (e) {
-    res.status(500).send();
+      res.status(500).send();
   }
 };
 
@@ -62,9 +55,8 @@ const profil = async (req, res) => {
 };
 
 const toutesRessourcesDeUtilisateur = async (req, res) => {
-  const user = await UtilisateurModel.find({ _id: req.params.id }).populate(
-    "ressources"
-  );
+  const user = await UtilisateurModel.find({ _id: req.params.id })
+    .populate('ressources');
   console.log(user);
   if (!user) {
     res.status(404).send("Cet utilisateur n'existe pas.");
@@ -102,17 +94,10 @@ const follow = async (req, res) => {
   if (!utilisateur) {
     res.status(404).send("Cet utilisateur n'existe pas.");
   } else {
-    var idexistant = await AbonnementModel.find({
-      utilisateur: utilisateur._id,
-      abonnement: "62a9da694dc6033c7098a68b",
-    });
-    if (idexistant == null || idexistant == "") {
+    var idexistant = await AbonnementModel.find({ utilisateur: utilisateur._id, abonnement: '62a9da694dc6033c7098a68b' });
+    if (idexistant == null || idexistant == '') {
       //ON CREE LE FOLLOW
-      const follow = new AbonnementModel({
-        _id: new mongoose.Types.ObjectId(),
-        utilisateur: utilisateur._id,
-        abonnement: "62a9da694dc6033c7098a68b",
-      });
+      const follow = new AbonnementModel({ _id: new mongoose.Types.ObjectId(), utilisateur: utilisateur._id, abonnement: '62a9da694dc6033c7098a68b' });
       await follow.save();
 
       // ON INCREMENTE LE NOMBRE D'ABONNE
@@ -120,10 +105,8 @@ const follow = async (req, res) => {
       await utilisateur.save();
 
       // ON INCREMENT LE NOMBRE D'ABONNEMENT
-      var abonnement = await UtilisateurModel.findById({
-        _id: "62a9da694dc6033c7098a68b",
-      });
-      abonnement.nbdabonnement++;
+      var abonnement = await UtilisateurModel.findById({ _id: '62a9da694dc6033c7098a68b' });
+      abonnement.nbdabonnement ++;
       await abonnement.save();
 
       res.send(follow, utilisateur, abonnement);
@@ -131,13 +114,11 @@ const follow = async (req, res) => {
       // On delete l'existance de l'id
       const unfollow = await AbonnementModel.findByIdAndDelete(idexistant);
       //On decremante le nombre dabonné
-      utilisateur.nbdabonne--;
+      utilisateur.nbdabonne --;
       await utilisateur.save();
 
       //On decremante le nombre dabonnement
-      var abonnement = await UtilisateurModel.findById({
-        _id: "62a9da694dc6033c7098a68b",
-      });
+      var abonnement = await UtilisateurModel.findById({ _id: '62a9da694dc6033c7098a68b' });
       abonnement.nbdabonnement--;
       await abonnement.save();
 
@@ -147,50 +128,51 @@ const follow = async (req, res) => {
 };
 
 const modifUtilisateur = async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["pseudo", "description", "mail", "mot_de_passe"];
-  const isValidOperation = updates.every((update) =>
-    allowedUpdates.includes(update)
-  );
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['pseudo', 'description', 'mail', 'mot_de_passe']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
   if (!isValidOperation) {
-    return res.status(400).send({ error: "Modifications invalides!" });
+      return res.status(400).send({ error: 'Modifications invalides!' })
   }
 
   try {
-    updates.forEach((update) => (req.utilisateur[update] = req.body[update]));
-    await req.utilisateur.save();
-    res.send(req.utilisateur);
+      updates.forEach((update) => req.utilisateur[update] = req.body[update])
+      await req.utilisateur.save()
+      res.send(req.utilisateur)
   } catch (e) {
-    res.status(400).send(e);
+      res.status(400).send(e)
   }
-};
+}
 
 const avatar = async (req, res) => {
   try {
-    const utilisateur = await UtilisateurModel.findById(req.params.id);
+      const utilisateur = await UtilisateurModel.findById(req.params.id)
 
-    if (!utilisateur || !utilisateur.image) {
-      throw new Error();
-    }
+      if (!utilisateur || !utilisateur.image) {
+          throw new Error()
+      }
 
-    res.set("Content-Type", "image/png");
-    res.send(utilisateur.image);
+      res.set('Content-Type', 'image/png')
+      res.send(utilisateur.image)
   } catch (e) {
-    res.status(404).send();
+      res.status(404).send()
   }
-};
+}
 
-module.exports = {
-  afficherUtilisateurs,
-  ajoutUtilisateur,
-  ajoutUtilisateurInscription,
-  Login,
-  Logout,
-  profil,
-  toutesRessourcesDeUtilisateur,
-  switchCompteUtilisateur,
-  follow,
-  modifUtilisateur,
-  avatar,
-};
+
+
+
+  module.exports= {
+    afficherUtilisateurs,
+    ajoutUtilisateur,
+    ajoutUtilisateurInscription,
+    Login,
+    Logout,
+    profil,
+    toutesRessourcesDeUtilisateur,
+    switchCompteUtilisateur,
+    follow,
+    modifUtilisateur,
+    avatar
+  };
--- routes/utilisateur.routes.js
+++ routes/utilisateur.routes.js
@@ -16,33 +16,14 @@ router.patch("utilisateur/:id/switch", auth, utilisateurController.switchCompteU
 router.patch("utilisateur/:id/follow", auth, utilisateurController.follow);
 router.patch("utilisateur/:id", auth, utilisateurController.modifUtilisateur);
 
-const storage = multer.diskStorage({
-  destination : (req, file, cb)=> {
-      cb(null, "./public")
-  },
-  filename : (req, file, cb)=> {
-      var date = new Date().toLocaleDateString();
-      cb(null, date+"-"+Math.round(Math.random() * 10000)+"-"+file.originalname)
-  }
-});
-const fileFilter = (req, file, cb) =>{
-  if(file.mimetype === "image/jpeg" || file.mimetype === "image/png"){
-      cb(null, true)
-  } else {
-      cb(new Error("l'image n'est pas acceptée"),false)
-  }
-}
-
 const upload = multer({
-  storage : storage,
-  limits : {
-      fileSize : 1024 * 1024 * 5
-  },
-  fileFilter : fileFilter
-})
+  dest: 'public'})
 
-router.get('/utilisateur/:id/image', auth, utilisateurController.avatar);
+router.post("/test",  upload.single("test"), (req, res) => {
+  res.send
+});
+router.post('/utilisateur/:id/image', auth, utilisateurController.avatar);
 
-router.post("/inscription", upload.single("image"), utilisateurController.ajoutUtilisateurInscription);
+router.post("/inscription", utilisateurController.ajoutUtilisateurInscription);
 
 module.exports = router;
